7fe42d32a50faaa9f752a46b83def4c9deb71f49
1n6ty <artyom.inety@gmail.com>
2024-02-29 03:12:39 +0700

    unet_LSTMConv model

--- Model/model.py
+++ Model/model.py
@@ -1,91 +1,107 @@
 import tensorflow as tf
 from tensorflow import keras
 from keras.models import Model
-from keras.layers import Input, ConvLSTM2D, Conv2D, Dropout, Concatenate, Reshape, Dense, MaxPooling2D, Conv2DTranspose
+from keras.layers import Input, ConvLSTM2D, Conv2D, Dropout, MaxPooling2D, Conv2DTranspose, BatchNormalization, Layer, concatenate, Softmax
+
+class AxileAttention(Layer):
+    def __init__(self, input_shape, mode): # mode = "vertical" | "horizontal"
+        super().__init__()
+
+        self.mode = mode
+        ind = 2 if mode == 'horizontal' else 1
+
+        self.query = self.add_weight(
+            initializer="random_normal",
+            trainable=True,
+            shape=[input_shape[0], input_shape[ind], input_shape[ind]]
+        )
+        self.key = self.add_weight(
+            initializer="random_normal",
+            trainable=True,
+            shape=[input_shape[0], input_shape[ind], input_shape[ind]]
+        )
+        self.var = self.add_weight(
+            initializer="random_normal",
+            trainable=True,
+            shape=[input_shape[0], input_shape[ind], input_shape[ind]]
+        )
+
+        self.query_b = self.add_weight(
+            initializer="random_normal",
+            trainable=True,
+            shape=input_shape
+        )
+        self.key_b = self.add_weight(
+            initializer="random_normal",
+            trainable=True,
+            shape=input_shape
+        )
+        self.var_b = self.add_weight(
+            initializer="random_normal",
+            trainable=True,
+            shape=input_shape
+        )
+
+        self.softmax_l = Softmax(axis = -1 if mode == 'horizontal' else -2)
+
+    def call(self, x):
+        if self.mode == "horizontal":
+            x = self.softmax_l(
+                (tf.matmul(x, self.query) + self.query_b) * (tf.matmul(x, self.key) + self.key_b)
+            ) * (tf.matmul(x, self.var) + self.var_b)
+        else:
+            x = self.softmax_l(
+                (tf.matmul(self.query, x) + self.query_b) * (tf.matmul(self.key, x) + self.key_b)
+            ) * (tf.matmul(self.var, x) + self.var_b)
+        
+        return x
 
 def conv_block(x, n_filter: int, kernel_size: tuple[int, int], dropout_rate = 0.1):
-    x = Conv2D(n_filter, kernel_size=kernel_size, padding="same", data_format="channels_last", kernel_initializer="he_normal", activation='elu')(x)
-    x = Dropout(dropout_rate)(x)
-    x = Conv2D(n_filter, kernel_size=kernel_size, padding="same", data_format="channels_last", kernel_initializer="he_normal", activation='elu')(x)
-    x = Dropout(dropout_rate)(x)
+    for _ in range(2):
+        x = Conv2D(n_filter, kernel_size=kernel_size, padding="same", data_format="channels_last", kernel_initializer="he_normal", activation='elu')(x)
+        x = Dropout(dropout_rate)(x)
+        x = BatchNormalization()(x)
     return x
 
-def axile_attention(x, x_shape: tuple[int, int], mode="width"): # shape: ([Batch_size,] height, width)
-    if mode == 'width':
-        dense_layer = Dense(x_shape[0], activation="softmax")
-        new_data = []
-        for i in range(x_shape[0]):
-            weights = dense_layer(x[:, i])
-
-            l = tf.multiply(x[:, i], weights)
-            l = tf.expand_dims(l, axis=1)
-
-            new_data.append(l)
-        
-        return Concatenate(axis=1)(new_data)
-    elif mode == 'height':
-        x = tf.transpose(x, perm=[0, 2, 1])
-        dense_layer = Dense(x_shape[1], activation="softmax")
-        new_data = []
-        for i in range(x_shape[1]):
-            weights = dense_layer(x[:, i])
-
-            l = tf.multiply(x[:, i], weights)
-            l = tf.expand_dims(l, axis=1)
-
-            new_data.append(l)
-        return tf.transpose(Concatenate(axis=1)(new_data), perm=[0, 2, 1])
-
-def make_unet_conv_LSTM_v2(input_shape: tuple[int, int, int, int], n_filters: int, kernel_size: tuple[int, int], out_seq: int, dropout_rate = 0.1):
+def make_unet_conv_LSTM(input_shape: tuple[int, int, int, int], n_filters: list, kernel_size: tuple[int, int], out_seq: int, n_attention: int, dropout_rate = 0.1):
     inp = Input(input_shape)
 
-    # Downsampling
-    down_conv_1 = [inp[:, i] for i in range(input_shape[0])]
+    # Unet structure before LSTM
+    unet_output = []
     for i in range(input_shape[0]):
-        down_conv_1[i] = conv_block(down_conv_1[i], n_filters, kernel_size, dropout_rate)
-        down_conv_1[i] = MaxPooling2D((2, 2), strides=(2, 2))(down_conv_1[i])
-    
-    down_conv_2 = [down_conv_1[i] for i in range(input_shape[0])]
-    for i in range(input_shape[0]):
-        down_conv_2[i] = conv_block(down_conv_2[i], n_filters * 2, kernel_size, dropout_rate)
-        down_conv_2[i] = MaxPooling2D((2, 2), strides=(2, 2))(down_conv_2[i])
-    
-    down_conv_3 = [down_conv_2[i] for i in range(input_shape[0])]
-    for i in range(input_shape[0]):
-        down_conv_3[i] = conv_block(down_conv_3[i], n_filters * 4, kernel_size, dropout_rate)
-        down_conv_3[i] = MaxPooling2D((2, 2), strides=(2, 2))(down_conv_3[i])
-    
-    for i in range(input_shape[0]):
-        down_conv_1[i] = tf.expand_dims(down_conv_1[i], axis=1)
-        down_conv_2[i] = tf.expand_dims(down_conv_2[i], axis=1)
-        down_conv_3[i] = tf.expand_dims(down_conv_3[i], axis=1)
-    
-    down_conv_1 = Concatenate(axis=1)(down_conv_1)
-    down_conv_2 = Concatenate(axis=1)(down_conv_2)
-    down_conv_3 = Concatenate(axis=1)(down_conv_3)
+        # Downsampling
+        conv_1_1 = conv_block(inp[:, i], n_filters[0], kernel_size, dropout_rate)
+        pool_1_1 = MaxPooling2D((2, 2))(conv_1_1)
 
-    # Encoder
-    conv_lstm_1 = [ConvLSTM2D(filters=n_filters, kernel_size=kernel_size, padding="same")(down_conv_1) for i in range(out_seq)]
-    conv_lstm_2 = [ConvLSTM2D(filters=n_filters * 2, kernel_size=kernel_size, padding="same")(down_conv_2) for i in range(out_seq)]
-    conv_lstm_3 = [ConvLSTM2D(filters=n_filters * 4, kernel_size=kernel_size, padding="same")(down_conv_3) for i in range(out_seq)]
-    conv_lstm_4 = [ConvLSTM2D(filters=n_filters * 8, kernel_size=kernel_size, padding="same")(down_conv_3) for i in range(out_seq)]
+        conv_2_1 = conv_block(pool_1_1, n_filters[1], kernel_size, dropout_rate)
+        pool_2_1 = MaxPooling2D((2, 2))(conv_2_1)
 
-    # Decoder
-    output = []
-    for seq in range(out_seq):
-        up_1 = Conv2DTranspose(n_filters * 4, (3, 3), strides=(2, 2), padding="same")(Concatenate(axis=-1)([conv_lstm_3[seq], conv_lstm_4[seq]]))
-        up_1 = Dropout(dropout_rate)(up_1)
+        conv_3 = conv_block(pool_2_1, n_filters[2], kernel_size, dropout_rate)
 
-        up_2 = Conv2DTranspose(n_filters * 2, (3, 3), strides=(2, 2), padding="same")(Concatenate(axis=-1)([conv_lstm_2[seq], up_1]))
-        up_2 = Dropout(dropout_rate)(up_2)
+        # Upsampling
+        conv_2_2 = conv_block(
+            concatenate([
+                conv_2_1,
+                Conv2DTranspose(n_filters[1], kernel_size, strides=(2, 2), padding="same", kernel_initializer="he_normal", activation="elu")(conv_3)
+            ]), n_filters[1], kernel_size, dropout_rate
+        )
+        conv_2_1 = conv_block(
+            concatenate([
+                conv_1_1,
+                Conv2DTranspose(n_filters[1], kernel_size, strides=(2, 2), padding="same", kernel_initializer="he_normal", activation="elu")(conv_2_2)
+            ]), n_filters[0], kernel_size, dropout_rate
+        )
+        unet_output.append(tf.expand_dims(conv_2_1, axis=1))
+    unet_output = concatenate(unet_output, axis=1)
 
-        up_3 = Conv2DTranspose(n_filters, (3, 3), strides=(2, 2), padding="same")(Concatenate(axis=-1)([conv_lstm_1[seq], up_2]))
-        up_3 = Dropout(dropout_rate)(up_3)
+    # LSTM
+    LSTM_output = tf.transpose(
+        ConvLSTM2D(out_seq, kernel_size, padding="same", kernel_initializer="he_normal")(unet_output), perm=[0, 3, 1, 2]
+    )
 
-        out = Conv2D(1, (3, 3), padding="same", kernel_initializer="he_normal", activation="linear")(up_3)
-        output.append(tf.expand_dims(out, axis=1))
-    
-    output = Concatenate(axis=1)(output)
-    output = Reshape((out_seq, input_shape[1], input_shape[2]))(output)
+    # Axile Attention
+    for i in range(n_attention):
+        LSTM_output = AxileAttention((out_seq, input_shape[1], input_shape[2]), "horizontal")(LSTM_output)
+        LSTM_output = AxileAttention((out_seq, input_shape[1], input_shape[2]), "vertical")(LSTM_output)
 
-    return Model(inputs = [inp], outputs = [output])
+    return Model(inputs = [inp], outputs = [LSTM_output])
